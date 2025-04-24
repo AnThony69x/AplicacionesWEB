b40128e27afde28c4ffc71a421b9dadc604b3cf7
AnThony69x <157445810+AnThony69x@users.noreply.github.com>
2025-04-23 22:04:28 -0500

    Modificar el mensaje de consola para que muestre "¡Hola mundo!"

--- PrimerParcial/Pactica1/Typescript/src/app.ts
+++ PrimerParcial/Pactica1/Typescript/src/app.ts
@@ -1,8 +1,7 @@
 let presentacion: string = "Hola mundo!";
 let edad: number = 22; 
-console.log("¡Hola desde TypeScript con ts-node-dev!");
+console.log("¡Hola mundo!");
 
-console.log("¡Hola desde TypeScript con ts-node-dev! 🎯");
 
 const saludar = (nombre: string) => {
     console.log(`Hola, ${nombre}!`);
