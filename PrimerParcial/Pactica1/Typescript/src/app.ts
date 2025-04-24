eb4030395813509fc8330e58d701c77164676468
AnThony69x <157445810+AnThony69x@users.noreply.github.com>
2025-04-23 21:58:59 -0500

    Inicialice el proyecto TypeScript con la configuración básica, incluyendo package.json, tsconfig.json y un archivo app.ts de ejemplo. Implemente una función de saludo y registros básicos de la consola.

---
+++ PrimerParcial/Pactica1/Typescript/src/app.ts
@@ -0,0 +1,11 @@
+let presentacion: string = "Hola mundo!";
+let edad: number = 22; 
+console.log("¡Hola desde TypeScript con ts-node-dev!");
+
+console.log("¡Hola desde TypeScript con ts-node-dev! 🎯");
+
+const saludar = (nombre: string) => {
+    console.log(`Hola, ${nombre}!`);
+};
+
+saludar("Anthony");
